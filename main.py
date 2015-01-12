5953636e62eff5ed23157a72785c8f7676d646d5
hiroki <functionp@gmail.com>
2015-01-12 23:01:36 +0900

    make current box instance valuable

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ WINDOW_SIZE = (1200,900)
 MAIN_WINDOW_SIZE = (900,550)
 MAIN_PADDING = (30,30)
 
-g_current_box = None
 g_main_frame = None
 
 class OptimizationFrame(wx.Frame):
@@ -41,10 +40,72 @@ class OptimizationFrame(wx.Frame):
 
 class MainFrame(wx.Frame):
 
-    def __init__(self, parent=None, id=-1, title=None, *args, **kwargs):
+    def __init__(self, current_box, parent=None, id=-1, title=None, *args, **kwargs):
         wx.Frame.__init__(self, parent, id, title, *args, **kwargs)
 
         self.base_panel = wx.Panel(self, wx.ID_ANY)
+        self.set_current_box(current_box)
+        self.refresh(self.current_box)
+
+    def set_current_box(self, current_box):
+        self.current_box = current_box
+
+    def refresh(self, target_box):
+        def get_right_position(basic_object, margin):
+            basic_x, basic_y = basic_object.Position
+            basic_width, basic_height = basic_object.Size
+            return [basic_x + basic_width + margin, basic_y]
+
+        def get_bottom_position(basic_object, margin):
+            basic_x, basic_y = basic_object.Position
+            basic_width, basic_height = basic_object.Size
+            return [basic_x, basic_y + basic_height + margin]
+
+        main_panel = wx.Panel(self.base_panel, wx.ID_ANY, pos=MAIN_PADDING, size=(MAIN_WINDOW_SIZE[0], MAIN_WINDOW_SIZE[1]))
+        print dir(main_panel)
+        print main_panel.Children
+
+        list_box = wx.ListBox(main_panel, wx.ID_ANY, choices=get_agent_list(target_box), style=wx.LB_SINGLE, pos=(2,2), size=(150, 300))
+
+        label_map = wx.StaticText(main_panel, wx.ID_ANY, target_box.identifier, pos=get_right_position(list_box, 10))
+
+        label_name = wx.StaticText(main_panel, wx.ID_ANY, "Name", pos=get_bottom_position(label_map, 10))
+        text_name = wx.TextCtrl(main_panel,wx.ID_ANY, target_box.identifier, pos=get_right_position(label_name, 5))
+
+        STYLE_TEXT_SIZE = (60,23)
+        label_x = wx.StaticText(main_panel, wx.ID_ANY, "X", pos=get_bottom_position(label_name, 10))
+        text_x = wx.TextCtrl(main_panel,wx.ID_ANY, str(target_box.get_x()) , pos=get_right_position(label_x, 5), size=STYLE_TEXT_SIZE)
+        text_x.SetMaxLength(4)
+
+        label_y = wx.StaticText(main_panel, wx.ID_ANY, "Y", pos=get_right_position(text_x, 20))
+        text_y = wx.TextCtrl(main_panel,wx.ID_ANY, str(target_box.get_y()), pos=get_right_position(label_y, 5), size=STYLE_TEXT_SIZE)
+        text_y.SetMaxLength(4)
+
+        label_width = wx.StaticText(main_panel, wx.ID_ANY, "Width", pos=get_right_position(text_y, 20))
+        text_width = wx.TextCtrl(main_panel,wx.ID_ANY, str(target_box.get_width()), pos=get_right_position(label_width, 5), size=STYLE_TEXT_SIZE)
+        text_width.SetMaxLength(4)
+
+        label_height = wx.StaticText(main_panel, wx.ID_ANY, "Height", pos=get_right_position(text_width, 20))
+        text_height = wx.TextCtrl(main_panel,wx.ID_ANY, str(target_box.get_height()), pos=get_right_position(label_height, 5), size=STYLE_TEXT_SIZE)
+        text_height.SetMaxLength(4)
+
+        make_button = wx.Button(main_panel, wx.ID_ANY, "Make New Box", pos=get_bottom_position(label_x, 30))
+        make_button.Bind(wx.EVT_BUTTON, click_make_button)
+
+        start_button = wx.Button(main_panel, wx.ID_ANY, "Start", pos=get_right_position(make_button, 10))
+        start_button.Bind(wx.EVT_BUTTON, click_start_button)
+
+        self.Show()
+        wx.Yield() 
+
+def main():
+    global g_main_frame
+
+    main_app = wx.App()
+    base_box =  BoxAgent(Style([0,0], WINDOW_SIZE, 0), "base")
+    g_main_frame = MainFrame(base_box, None, -1, u'controller', pos=(100,100),size=MAIN_WINDOW_SIZE)
+
+    main_app.MainLoop()
 
 def get_agent_list(box):
     if box.inner_layout:
@@ -52,72 +113,14 @@ def get_agent_list(box):
     else:
         return []
 
-def reflesh_main(target_box):
-    def get_right_position(basic_object, margin):
-        basic_x, basic_y = basic_object.Position
-        basic_width, basic_height = basic_object.Size
-        return [basic_x + basic_width + margin, basic_y]
-        
-    def get_bottom_position(basic_object, margin):
-        basic_x, basic_y = basic_object.Position
-        basic_width, basic_height = basic_object.Size
-        return [basic_x, basic_y + basic_height + margin]
 
-    main_panel = wx.Panel(g_main_frame.base_panel, wx.ID_ANY, pos=MAIN_PADDING, size=(MAIN_WINDOW_SIZE[0], MAIN_WINDOW_SIZE[1]))
-
-    list_box = wx.ListBox(main_panel, wx.ID_ANY, choices=get_agent_list(target_box), style=wx.LB_SINGLE, pos=(2,2), size=(150, 300))
-
-    label_map = wx.StaticText(main_panel, wx.ID_ANY, target_box.identifier, pos=get_right_position(list_box, 10))
-
-    label_name = wx.StaticText(main_panel, wx.ID_ANY, "Name", pos=get_bottom_position(label_map, 10))
-    text_name = wx.TextCtrl(main_panel,wx.ID_ANY, target_box.identifier, pos=get_right_position(label_name, 5))
-
-    STYLE_TEXT_SIZE = (60,23)
-    label_x = wx.StaticText(main_panel, wx.ID_ANY, "X", pos=get_bottom_position(label_name, 10))
-    text_x = wx.TextCtrl(main_panel,wx.ID_ANY, str(target_box.get_x()) , pos=get_right_position(label_x, 5), size=STYLE_TEXT_SIZE)
-    text_x.SetMaxLength(4)
-
-    label_y = wx.StaticText(main_panel, wx.ID_ANY, "Y", pos=get_right_position(text_x, 20))
-    text_y = wx.TextCtrl(main_panel,wx.ID_ANY, str(target_box.get_y()), pos=get_right_position(label_y, 5), size=STYLE_TEXT_SIZE)
-    text_y.SetMaxLength(4)
-
-    label_width = wx.StaticText(main_panel, wx.ID_ANY, "Width", pos=get_right_position(text_y, 20))
-    text_width = wx.TextCtrl(main_panel,wx.ID_ANY, str(target_box.get_width()), pos=get_right_position(label_width, 5), size=STYLE_TEXT_SIZE)
-    text_width.SetMaxLength(4)
-
-    label_height = wx.StaticText(main_panel, wx.ID_ANY, "Height", pos=get_right_position(text_width, 20))
-    text_height = wx.TextCtrl(main_panel,wx.ID_ANY, str(target_box.get_height()), pos=get_right_position(label_height, 5), size=STYLE_TEXT_SIZE)
-    text_height.SetMaxLength(4)
-
-    make_button = wx.Button(main_panel, wx.ID_ANY, "Make New Box", pos=get_bottom_position(label_x, 30))
-    make_button.Bind(wx.EVT_BUTTON, click_make_button)
-
-    start_button = wx.Button(main_panel, wx.ID_ANY, "Start", pos=get_right_position(make_button, 10))
-    start_button.Bind(wx.EVT_BUTTON, click_start_button)
-
-    g_main_frame.Show()
-    wx.Yield() 
-
-
-def main():
-    global g_current_box
-    global g_main_frame
-
-    main_app = wx.App()
-    g_main_frame = MainFrame(None, -1, u'controller', pos=(100,100),size=MAIN_WINDOW_SIZE)
-
-    g_current_box =  BoxAgent(Style([0,0], WINDOW_SIZE, 0), "base")
-
-    reflesh_main(g_current_box)
-    main_app.MainLoop()
 
 def click_make_button(event):
-    global g_current_box
     new_box = BoxAgent(Style([0,0], [0,0], 1), "new box")
-    Layout([new_box], g_current_box)
+    Layout([new_box], g_main_frame.current_box)
 
-    g_current_box = new_box
-    reflesh_main(new_box)
+    g_main_frame.set_current_box(new_box)
+    g_main_frame.refresh(new_box)
 
 def click_start_button(event):
     def render_closure(layout):
