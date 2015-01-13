ec12c7efb8438960b817a2fe688149058c249c62
hiroki <functionp@gmail.com>
2015-01-13 15:47:39 +0900

    bug fix

--- main.py
+++ main.py
@@ -84,44 +84,47 @@ class MainFrame(wx.Frame):
 
         main_panel = wx.Panel(self.base_panel, wx.ID_ANY, pos=MAIN_PADDING, size=(MAIN_WINDOW_SIZE[0], MAIN_WINDOW_SIZE[1]))
 
-        list_box = wx.ListBox(main_panel, wx.ID_ANY, choices=get_agent_list(target_box), style=wx.LB_SINGLE, pos=(2,2), size=(150, 300))
+        for child in g_widgets.values():
+            child.Destroy()
+
+        list_box = widget_factory(wx.ListBox, main_panel, 15, choices=get_agent_list(target_box), style=wx.LB_SINGLE, pos=(2,2), size=(150, 300))
         list_box.Bind(wx.EVT_LISTBOX, select_list_box)
 
-        if get_widget_by_id(222): 
-            get_widget_by_id(222).SetLabel("")
+        if get_widget_by_id(0): 
+            get_widget_by_id(0).SetLabel("")
 
-        label_map = widget_factory(wx.StaticText, main_panel, 222, get_map(target_box), pos=get_right_position(list_box, 10))
+        label_map = widget_factory(wx.StaticText, main_panel, 0, get_map(target_box), pos=get_right_position(list_box, 10))
 
-        label_name = wx.StaticText(main_panel, wx.ID_ANY, "Name", pos=get_bottom_position(label_map, 10))
+        label_name = widget_factory(wx.StaticText, main_panel, 6, "Name", pos=get_bottom_position(label_map, 10))
         text_name = widget_factory(wx.TextCtrl, main_panel, 1, target_box.identifier, pos=get_right_position(label_name, 5))
 
         STYLE_TEXT_SIZE = (60,23)
-        label_x = wx.StaticText(main_panel, wx.ID_ANY, "X", pos=get_bottom_position(label_name, 10))
+        label_x = widget_factory(wx.StaticText, main_panel, 7, "X", pos=get_bottom_position(label_name, 10))
         text_x = widget_factory(wx.TextCtrl, main_panel, 2, str(target_box.get_x()) , pos=get_right_position(label_x, 5), size=STYLE_TEXT_SIZE)
         text_x.SetMaxLength(4)
 
-        label_y = wx.StaticText(main_panel, wx.ID_ANY, "Y", pos=get_right_position(text_x, 20))
+        label_y = widget_factory(wx.StaticText, main_panel, 8, "Y", pos=get_right_position(text_x, 20))
         text_y = widget_factory(wx.TextCtrl, main_panel, 3, str(target_box.get_y()), pos=get_right_position(label_y, 5), size=STYLE_TEXT_SIZE)
         text_y.SetMaxLength(4)
 
-        label_width = wx.StaticText(main_panel, wx.ID_ANY, "Width", pos=get_right_position(text_y, 20))
+        label_width = widget_factory(wx.StaticText, main_panel, 9, "Width", pos=get_right_position(text_y, 20))
         text_width = widget_factory(wx.TextCtrl, main_panel, 4, str(target_box.get_width()), pos=get_right_position(label_width, 5), size=STYLE_TEXT_SIZE)
         text_width.SetMaxLength(4)
 
-        label_height = wx.StaticText(main_panel, wx.ID_ANY, "Height", pos=get_right_position(text_width, 20))
+        label_height = widget_factory(wx.StaticText, main_panel, 10, "Height", pos=get_right_position(text_width, 20))
         text_height = widget_factory(wx.TextCtrl, main_panel, 5, str(target_box.get_height()), pos=get_right_position(label_height, 5), size=STYLE_TEXT_SIZE)
         text_height.SetMaxLength(4)
 
-        make_button = wx.Button(main_panel, wx.ID_ANY, "Make New Box", pos=get_bottom_position(label_x, 30))
+        make_button = widget_factory(wx.Button, main_panel, 11, "Make New Box", pos=get_bottom_position(label_x, 30))
         make_button.Bind(wx.EVT_BUTTON, click_make_button)
 
-        upper_button = wx.Button(main_panel, wx.ID_ANY, "Upper Layer", pos=get_right_position(make_button, 10))
+        upper_button = widget_factory(wx.Button, main_panel, 12, "Upper Layer", pos=get_right_position(make_button, 10))
         upper_button.Bind(wx.EVT_BUTTON, click_upper_button)
 
-        update_button = wx.Button(main_panel, wx.ID_ANY, "Update", pos=get_right_position(upper_button, 10))
+        update_button = widget_factory(wx.Button, main_panel, 13, "Update", pos=get_right_position(upper_button, 10))
         update_button.Bind(wx.EVT_BUTTON, click_update_button)
 
-        start_button = wx.Button(main_panel, wx.ID_ANY, "Start", pos=get_right_position(update_button, 10))
+        start_button = widget_factory(wx.Button, main_panel, 14, "Start", pos=get_right_position(update_button, 10))
         start_button.Bind(wx.EVT_BUTTON, click_start_button)
 
         self.Show()
